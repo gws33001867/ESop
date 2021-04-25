c95499fdb3de5bbea992d8040001b9c575969c39
LanceShan <965250499@qq.com>
2021-04-25 08:17:14 +0800

    update eureka注册中心搭建，多模块搭建

--- eureka_server/src/main/java/com/sp/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/sp/eureka_server/EurekaServerApplication.java
@@ -3,7 +3,10 @@ package com.sp.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
@@ -12,4 +15,8 @@ public class EurekaServerApplication {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
+    @GetMapping("/hi")
+    public String hi(){
+        return "Hello World";
+    }
 }
