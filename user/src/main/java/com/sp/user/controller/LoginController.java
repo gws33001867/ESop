677ac37b7718089fb61d9b732bd18b351f34551b
lance <965250499@qq.com>
2021-05-14 10:59:28 +0800

    新增sentinel

--- user/src/main/java/com/sp/user/controller/LoginController.java
+++ user/src/main/java/com/sp/user/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RefreshScope
 public class LoginController {
@@ -15,5 +17,20 @@ public class LoginController {
     @GetMapping("/config")
     public String getWife(){
         return wife;
+    }
+
+    @GetMapping("/testA")
+    public String testA(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "-----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
     }
 }
